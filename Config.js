1f23e6d6d0db98a605d531020327bfad1ebf9040
Rexconc <rekpenew@gmail.com>
2025-01-14 23:12:44 +0100

    Update Config.js

--- Config.js
+++ Config.js
@@ -4,7 +4,7 @@ global.settings = {
   OWNER_NUMBERS: "2348142062575", // your number with country code
   OWNER_NAME: 'daniel', // your owner name 
   BOT_NAME: "KORD", // your preferred bot name
-  SESSION_ID: 'kord_ai-wUCRhC0WoFwn', // replace your_sessionid with yours and make sure its inside rhe quotes
+  SESSION_ID: 'kord_ai-nPu4h88cwd1i', // replace your_sessionid with yours and make sure its inside rhe quotes
   INVINCIBLE_MODE: false, // toogle invincible mode 
   WORK_MODE: "private",
   TIME_ZONE: "Africa/Lagos",
